e41cc188d6f3dfebaecbf6ada7d355338aefbbeb
Ukd1796 <ujjwal.kumar@somaiya.edu>
2023-01-04 14:16:41 +0530

    splitting the app intwo widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/material.dart';
+import './widgets/user_transaction.dart';
 
-import './transaction.dart';
-import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,20 +15,9 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transcations = [
-    Transaction(
-      id: 't1',
-      title: 'task1',
-      amount: 69.90,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'task2',
-      amount: 99.90,
-      date: DateTime.now(),
-    )
-  ];
+//  late String titleInput;
+//  late String amountInput;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,50 +36,7 @@ class MyHomePage extends StatelessWidget {
             ),
             elevation: 5,
           ),
-          Column(
-            children: transcations.map((tx) {
-              return Card(
-                child: Row(children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 50,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2,
-                      ),
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'Rs ${tx.amount}', // this form of writing a string is known as string interpolation
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple[200]),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(tx.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          )),
-                      Text(
-                        DateFormat.yMMMd().format(tx.date),
-                        style: TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ]),
-              );
-            }).toList(),
-          ),
+          UserTransactions()
         ],
       ),
     );
---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: transactions.map((tx) {
+        return Card(
+          child: Row(children: <Widget>[
+            Container(
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 50,
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 2,
+                ),
+              ),
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Rs ${tx.amount}', // this form of writing a string is known as string interpolation
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple[200]),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(tx.title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    )),
+                Text(
+                  DateFormat.yMMMd().format(tx.date),
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ]),
+        );
+      }).toList(),
+    );
+  }
+}
